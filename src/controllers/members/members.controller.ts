e544763babe386551aa8044f202794aa3c6d638d
ISC-Lorenz <isc.lorenzovr@gmail.com>
2026-03-05 15:52:37 -0600

    feat: Member searching by filter implemented

--- src/controllers/members/members.controller.ts
+++ src/controllers/members/members.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { Auth } from '../../decorators/auth.decorator';
 import { MemberRequest } from '../../dtos/requests/member.request';
+import { PaginationOptions } from '../../dtos/requests/pagination-options';
 
 @Controller('members')
 export class MembersController {
@@ -11,5 +12,11 @@ export class MembersController {
   @Auth()
   public async createMember(@Body() body: MemberRequest, @Req() req: Request){
     await this.membersService.createNewMember(body, req['user'].identifier);
+  }
+
+  @Get()
+  @Auth()
+  public async searchMember (@Query() query: PaginationOptions){
+    return await this.membersService.findMembersByFilter(query.page, query.limit, query.order, query.filter)
   }
 }
